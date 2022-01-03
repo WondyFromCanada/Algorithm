4a5f9e2d83c6a4f8b064a0d6a3e1ca3b1e80c035
Wondy Kim <wondy96@gmail.com>
2022-01-04 00:47:45 +0900

    My First Algorithm Project
      - 1. 프로젝트 생성
      - 2. 프로그래머스 Level1 : 직사각형 별찍기

---
+++ src/com/algo/level1/RectangleStar.java
@@ -0,0 +1,23 @@
+package com.algo.level1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+public class RectangleStar {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		IntStream.range(0, n).forEach(s -> sb.append("*"));
+		IntStream.range(0, m).forEach(s -> System.out.println(sb));
+	}
+
+}
