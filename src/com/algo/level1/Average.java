89e019af0dd69bc5b7f81ec1e9b6ab30a5cc48d7
Wondy Kim <wondy96@gmail.com>
2022-01-05 00:42:50 +0900

    프로그래머스 Level1
      - 하샤드 수
      - 평균 구하기

---
+++ src/com/algo/level1/Average.java
@@ -0,0 +1,22 @@
+package com.algo.level1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class Average {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int arr[] = Pattern.compile(" ").splitAsStream(s).mapToInt(Integer::parseInt).toArray();
+		
+		System.out.println(solution(arr));
+	}
+	
+	public static double solution(int[] arr) {
+		return Arrays.stream(arr).average().getAsDouble();
+	}
+
+}
---
+++ src/com/algo/level1/HarshadNumber.java
@@ -0,0 +1,26 @@
+package com.algo.level1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class HarshadNumber {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int x = Integer.parseInt(br.readLine());
+		System.out.println(solution(x));
+	}
+	
+	public static boolean solution(int x) {
+        int sum = 0;
+        String s = String.valueOf(x);
+        
+        for(int i = 0; i < s.length(); i++) {
+        	sum += s.charAt(i) - '0';
+        }
+        
+        return (x % sum == 0);
+    }
+
+}
