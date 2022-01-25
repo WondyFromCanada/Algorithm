090b6fd8e54fb49963252233afc36793fd68eff9
wondy <wondy96@kaoni.com>
2022-01-26 01:01:56 +0900

    프로그래머스 level1
      - 문자열 내림차순으로 배치하기

---
+++ src/com/algo/level1/StringDesc.java
@@ -0,0 +1,28 @@
+package com.algo.level1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class StringDesc {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		
+		System.out.println(solution(s));
+	}
+
+	private static String solution(String s) {
+		String sarr[] = s.split("");
+		Arrays.sort(sarr);
+		StringBuilder sb = new StringBuilder();
+		
+		for(String st : sarr) {
+			sb.append(st);
+		}
+		
+		return sb.reverse().toString();
+	}
+
+}
